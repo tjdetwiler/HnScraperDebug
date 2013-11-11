861ce1f63e2d210b164ff8b40559fb5191a4db2f
Tim Detwiler <timdetwiler@gmail.com>
2013-11-10 20:06:43 -0500

    Add login support

--- src/main/java/com/detwiler/hackernews/debug/HnScraperDebug.java
+++ src/main/java/com/detwiler/hackernews/debug/HnScraperDebug.java
@@ -1,26 +1,53 @@
 package com.detwiler.hackernews.debug;
 
-import com.detwiler.hackernews.HnPost;
+import com.detwiler.hackernews.AuthenticationException;
+import com.detwiler.hackernews.model.HnComment;
 import com.detwiler.hackernews.HnPostCategory;
 import com.detwiler.hackernews.HnScraper;
+import com.detwiler.hackernews.model.HnSubmission;
+import com.detwiler.hackernews.server.HnPostDocument;
 import com.detwiler.hackernews.server.HnPostListDocument;
+import com.detwiler.hackernews.server.HnSessionManager;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HnScraperDebug {
+public class HnScraperDebug implements Runnable, HnSessionManager.CredentialDelegate {
+    private Map<String, String> mCredentials;
 
-    private static void dumpDocument(final HnPostListDocument doc) {
-        for (HnPost post : doc.getPosts()) {
+    public HnScraperDebug() {
+        mCredentials = new HashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("HnScraperDebug/src/main/resources/accounts.cfg")));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] userpass = line.split(",");
+                mCredentials.put(userpass[0].trim(), userpass[1].trim());
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (final Exception e) {
+            System.out.println("Exception while loading credentials: " + e);
+            System.exit(1);
+        }
+    }
+
+    private void dumpDocument(final HnPostListDocument doc) {
+        for (HnSubmission post : doc.getPosts()) {
             System.out.println(post);
         }
         System.out.println("More: " + doc.getNextPageHref());
     }
 
-    public static void main(String[] args) {
-        HnScraper scraper = new HnScraper();
+    private void readPostLists(final HnScraper scraper) {
         HnPostListDocument doc;
         try {
-            for (final HnPostCategory category : HnPostCategory.values()) {
+            HnPostCategory[] categories = HnPostCategory.values();
+            for (final HnPostCategory category : categories) {
                 System.out.println("Reading category: " + category);
                 doc = scraper.getPostsForCategory(category);
                 for (int i=0; i<1; ++i) {
@@ -36,5 +63,53 @@ public class HnScraperDebug {
             System.exit(-1);
             return;
         }
+    }
+
+    private static void readPost(final HnScraper scraper, final String id) {
+        HnPostDocument doc;
+        try {
+            doc = scraper.getPost(id);
+            for (final HnComment comment : doc.getComments()) {
+                int depth = 0;
+                HnComment parent = comment.getParent();
+                while (parent != null) {
+                    depth++;
+                    parent = parent.getParent();
+                }
+                String indent = "";
+                for (int i=0; i<depth; ++i) {
+                    indent += "\t";
+                }
+                System.out.println(indent + "Comment '" + comment.getPostId() + "' by " + comment.getUsername() + ":");
+                System.out.println(indent + comment.getText());
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: Unable to connect to HN server");
+            System.exit(-1);
+            return;
+        }
+    }
+
+    @Override
+    public void run() {
+        HnScraper scraper = new HnScraper();
+        scraper.setCredentialDelegate(this);
+        try {
+            scraper.setActiveUser("tjdetwiler");
+        } catch (final AuthenticationException e) {
+            System.out.println("Unable to authenticate user: " + e);
+            System.exit(1);
+        }
+        // readPost(scraper, "6706065");
+        readPostLists(scraper);
+    }
+
+    public static void main(String[] args) {
+        new HnScraperDebug().run();
+    }
+
+    @Override
+    public String getPasswordForUser(String username) {
+        return mCredentials.get(username);
     }
 }
